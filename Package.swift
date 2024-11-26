60cd1baf12876de82c7279c12f0f71a54ed3f29f
Hen Sh <hshabat@medallia.com>
2024-11-26 11:51:49 +0200

    iOS 13 minimum update

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MedalliaDigitalSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "MedalliaDigitalSDK",
