2ffa3b2418375c3a1d8d0c3e6fc2167ca1157ab5
Ori Sarig <osarig@medallia.com>
2025-11-11 15:50:39 +0200

    MDMS-14238 Make the SDK Bridge an internal dependency (#13)
    MDMS-14238 Make the SDK Bridge an internal dependency

---
+++ MedalliaDigitalSDKWrapper/dummy.swift
@@ -0,0 +1,5 @@
+//
+// MedalliaDigitalSDK
+// Copyright © 2025 Medallia. Use subject to licensing terms.
+//
+// Swift Package Manager needs at least one header file.
--- Package.swift
+++ Package.swift
@@ -5,18 +5,39 @@ import PackageDescription
 
 let package = Package(
     name: "MedalliaDigitalSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "MedalliaDigitalSDK",
-            targets: ["MedalliaDigitalSDK"]),
+            targets: ["MedalliaDigitalSDKWrapper"]
+        ),
     ],
     dependencies: [
-        
+        .package(
+            name: "MedalliaBridgeSDK",
+            url: "https://github.com/medallia/mobile-ios-bridge-sdk.git",
+            .upToNextMajor(from: "1.0.0")
+        )
     ],
     targets: [
         .binaryTarget(
             name: "MedalliaDigitalSDK",
             path: "artifacts/MedalliaDigitalSDK.xcframework"
         ),
+        .target(
+            name: "MedalliaDigitalSDKWrapper",
+            dependencies: [
+                .target(
+                    name: "MedalliaDigitalSDK"
+                ),
+                .product(
+                    name: "medallia-mobile-bridge-ios-sdk",
+                    package: "MedalliaBridgeSDK"
+                )
+            ],
+            path: "MedalliaDigitalSDKWrapper"
+        )
     ]
 )
